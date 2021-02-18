9549eea5b98d80a856b1f11179e4ddbc755117f4
Matheus Kroning <kroningmatheus@gmail.com>
2021-02-18 16:40:23 -0300

    :lipstick: Update color
    Update color

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,9 +1,9 @@
 const theme = {
   colors: {
     primary:"#fff",
-    secondary:"#c0c",
-    text: "#F7Cf",
-    hover: "#fff"
+    secondary:"#c0c0c0",
+    text: "#EAF7CF",
+    hover: "#c0c"
   }
 }
 
