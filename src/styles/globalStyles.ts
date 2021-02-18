5b1c1b77bdf28e62a88c77797c3be3102916a8e4
Matheus Kroning <kroningmatheus@gmail.com>
2021-02-18 16:26:53 -0300

    :lipstick: Add styles
    Add styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import bgPng from "../assets/bg.png";
+
+export default createGlobalStyle`
+  body {
+    margin: 0;
+
+    font-family: 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+
+  background-image: url(${bgPng});
+  background-size: cover;
+  background-position: top;
+  background-attachment: fixed;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  }
+`;
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    primary:"#000",
+    secondary:"#c0c",
+    text: "#F7Cf",
+    hover: "#fff"
+  }
+}
+
+export default theme;
